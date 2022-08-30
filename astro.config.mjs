654d3a0ee8866868996f24a53fb478d030c35f5a
CodeAvenger123 <alecferriby@gmail.com>
2022-08-30 21:39:37 +0100

    Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: 'netlify()',
+  adapter: netlify(),
   integrations: [tailwind()]
 });
 
